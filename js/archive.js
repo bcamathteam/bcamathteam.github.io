eff1e190684a620d1962fe61e5ac8ccf3f72dc2f
dknj11902 <dankim20@bergen.org>
2019-09-29 17:15:48 -0400

    Integrated Firebase

--- js/archive.js
+++ js/archive.js
@@ -1,25 +1,37 @@
 (function ($) {
     $(function () {
 
-        
-        $.getJSON('https://gist.githubusercontent.com/dknj11902/9c145d4aa2b177d0671555e016666d2d/raw/handouts.json', function(data) {
+        // var testRef = firebase.database().ref().child("Test");
+
+        // $("#firebasetest").click(function () {
+        //     testRef.child("Key1").once('value').then(function(snapshot) {
+        //         console.log(snapshot.val());
+        //     });
+        // });
+
+        var handoutsRef = firebase.database().ref('handouts/');
+
+        handoutsRef.once('value', function (snapshot) {
             var latest_count = 0;
             var latest_date = "";
-            $.each(data["handouts"].reverse(), function(i,v) {
+            snapshot.forEach(function (childSnapshot) {
+                var i = childSnapshot.key;
+                var v = childSnapshot.val();
                 if (i === 0) {
                     latest_date = v["date"];
                 }
                 if (v["date"] === latest_date) {
                     latest_count++;
+                } else {
+                    latest_count = 1;
+                    latest_date = v["date"];
                 }
-                var new_tr = "<tr><td>"+v["date"]+"</td><td><a href=\"/assets/handouts/"+v["file"]+"\" target=\"_blank\">"+v["title"]+"</a>"+(v["advanced"] ? "&nbsp;&nbsp;<span class=\"new badge red\" data-badge-caption=\"Advanced\"></span>" : "")+"</td></tr>";
-                $(".handouts-table tbody").append(new_tr)
+                var new_tr = "<tr><td>" + v["date"] + "</td><td><a href=\"/assets/handouts/" + v["file"] + "\" target=\"_blank\">" + v["title"] + "</a>" + (v["advanced"] ? "&nbsp;&nbsp;<span class=\"new badge red\" data-badge-caption=\"Advanced\"></span>" : "") + "</td></tr>";
+                $(".handouts-table tbody").prepend(new_tr);
             });
-            var new_badge = "&nbsp;&nbsp;<span class=\"new badge\">"+latest_count+"</span>";
-            $('.handouts-table thead tr td:nth-of-type(2)').html($('.handouts-table thead tr td:nth-of-type(2)').html()+new_badge);
+            var new_badge = "&nbsp;&nbsp;<span class=\"new badge\">" + latest_count + "</span>";
+            $('.handouts-table thead tr td:nth-of-type(2)').html($('.handouts-table thead tr td:nth-of-type(2)').html() + new_badge);
         });
-
-        
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
--- js/news.js
+++ js/news.js
@@ -1,19 +1,24 @@
 (function ($) {
     $(function () {
-        $.getJSON('https://gist.githubusercontent.com/dknj11902/9c145d4aa2b177d0671555e016666d2d/raw/updates.json', function(data) {         
-            $.each(data["updates"].reverse(), function(i,v) {
+        var updatesRef = firebase.database().ref('updates/');
+
+        updatesRef.once('value', function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+                var i = childSnapshot.key;
+                var v = childSnapshot.val();
                 var new_tr = `<div class="col s12 m6">
                 <div class="card orange lighten-3 z-depth-3 ">
                     <div class="card-content black-text">
-                        <span class="card-title">`+v["title"] + " (" + v["date"] + ")" + (i == 0 ? "<span class=\"new badge\"></span>" : "") + `</span>
-                        <p>`+v["content"]+`</p>`
-                
-                $.each(v["files"], function(ii,vv) {
-                    new_tr += `<a target="_blank" href="/assets/handouts/`+vv+`" class="waves-effect waves-light btn"><i class="material-icons left">file_download</i>`+vv+`</a>`
-                });
-                
+                        <span class="card-title">`+ v["title"] + " (" + v["date"] + ")" + (i == snapshot.numChildren() - 1 ? "<span class=\"new badge\"></span>" : "") + `</span>
+                        <p>`+ v["content"] + `</p>`
+                if (v["files"]) {
+                    $.each(v["files"], function (ii, vv) {
+                        new_tr += `<a target="_blank" href="/assets/handouts/` + vv + `" class="waves-effect waves-light btn"><i class="material-icons left">file_download</i>` + vv + `</a>`
+                    });
+                }
+
                 new_tr += `</div></div></div>`;
-                $(".updates-row").append(new_tr)
+                $(".updates-row").prepend(new_tr);
             });
         });
 
