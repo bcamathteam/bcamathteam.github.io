8988b16d6f85401bd860a60796c1b8155f062c0b
dknj11902 <dankim20@bergen.org>
2020-04-25 18:53:03 -0400

    added 2nd blog post

--- js/blog.js
+++ js/blog.js
@@ -1,10 +1,13 @@
 var list_of_files = [];
 
+/*
+Find: \\textbf\{([^}]*)\}
+Replace: \\textbf{$1}
+*/
+
 function waitForElementToDisplay(selector, time) {
-    console.log(selector);
     if (document.querySelector(selector) != null) {
         $(selector).modal();
-        console.log(`$(selector).modal();`)
         return;
     }
     else {
@@ -16,16 +19,17 @@ function waitForElementToDisplay(selector, time) {
 
 (function ($) {
     $(function () {
+
         fetch('assets/list_of_blogs.txt').then(response => response.text()).then(function (text) {
             var lines = text.split('\n');
             for (var j = 0; j < lines.length; j++) {
                 list_of_files.push(lines[j]);
             }
         }).then(function () {
-            for (var i = 0; i < list_of_files.length; i++) {
+            list_of_files.forEach(function (listItem, i) {
                 var current_id = i;
+                var title, author, date, preview, content = "";
                 fetch('blogs/' + list_of_files[i] + '.txt').then(response => response.text()).then(function (text) {
-                    var title, author, date, preview, content = "";
                     var lines = text.split('\n');
                     for (var j = 0; j < lines.length; j++) {
                         var line = lines[j];
@@ -48,36 +52,14 @@ function waitForElementToDisplay(selector, time) {
                             break;
                         }
                     }
-                    console.log(content);
-                    $("#blog_posts").append(`<div id="modal` + current_id + `" class="modal full">
-                <div class="modal-content"><div class="container">
-                    <h4>`+ title + `</h4>
-                    <h6>By `+ author + `</h6> <i class="material-icons">access_time</i> Updated ` + date + `<br><br>` + content + `
-                </div></div>
-                <div class="modal-footer">
-                <div class="container">
-                    <a href="#!" class="modal-close waves-effect waves-green btn orange lighten-3 black-text">Close</a>
-                </div></div>
-            </div>`);
-                    $("#blog_posts").append(`<div class="col s12 m6">
-                <div class="card orange lighten-3 z-depth-2 hoverable">
-                    <div class="card-content black-text">
-                        <span class="card-title">`+ title + `</span>
-                        <h6>By `+ author + `</h6>
-                        <p><i class="material-icons">access_time</i> Updated `+ date + `</p>
-                        <br>
-                        <p>`+ preview + `</p>
-                    </div>
-                    <div class="card-action">
-                        <a class="black-text modal-trigger" href="#modal`+ current_id + `">Read more</a>
-                    </div>
-                </div>
-            </div>`)
+                }).then(function () {
+                    $("#blog_posts").append(`<div id="modal` + current_id + `" class="modal full"><div class="modal-content"><div class="container"><h4>` + title + `</h4><h6>By ` + author + `</h6> <i class="material-icons">access_time</i> Updated ` + date + `<br><br>` + content + `</div></div><div class="modal-footer"><div class="container"><a href="#!" class="modal-close waves-effect waves-green btn orange lighten-3 black-text">Close</a></div></div></div>`);
+                    $("#blog_posts").append(`<div class="col s12 m6"><div class="card orange lighten-3 z-depth-2 hoverable"><div class="card-content black-text"><span class="card-title">` + title + `</span><h6>By ` + author + `</h6><p><i class="material-icons">access_time</i> Updated ` + date + `</p><br><p>` + preview + `</p></div><div class="card-action"><a class="black-text modal-trigger" href="#modal` + current_id + `">Read more</a></div></div></div>`)
                 });
-            }
-            for (i = 0; i < list_of_files.length; i++) {
-                console.log('#modal' + i);
-                waitForElementToDisplay('#modal' + i, 100);
+            });
+            for (var m = 0; m < list_of_files.length; m++) {
+                console.log('#modal' + m);
+                waitForElementToDisplay('#modal' + m, 100);
             }
             $('body').append(`<script src="/js/blog_modal.js"></script>`);
         });
